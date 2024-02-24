662dc5d0a8b726848f49585146c474d6b01f58c6
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-24 13:15:57 +0530

    class object baharwala class

--- ClassObj.java
+++ ClassObj.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
+class baharWalaClass {
+
+    public int num;
+    public int age;
+}
+
 public class ClassObj {
 
     String Name;
     int age;
     
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        baharWalaClass obj2 = new baharWalaClass();
+        obj2.num = input.nextInt();
+        obj2.age = input.nextInt();
+        System.out.println("Name " + obj2.num);
+        System.out.println("Age: " + obj2.age);
+
         ClassObj obj1 = new ClassObj();
-
-        obj1.Name = "Vinayak";
-        obj1.age = 30;
-
+        obj1.Name = input.next();
+        obj1.age = input.nextInt();
         System.out.println("Name " + obj1.Name);
         System.out.println("Age: " + obj1.age);
+
+        input.close();
     }
 }
