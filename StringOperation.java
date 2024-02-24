718b370cab2b5de3989477f12eefd58dc940a3e9
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-24 12:58:50 +0530

    String commit

---
+++ StringOperation.java
@@ -0,0 +1,13 @@
+public class StringOperation {
+    
+    public static void main(String[] args) {
+        
+        String str = new String("Sahu");
+
+        System.out.println(str);
+        
+        str = "Vinayak";
+
+        System.out.println(str);
+    }
+}
