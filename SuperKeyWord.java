66fd9071c678975d2a8df5325174ac0db5f6244c
Vinayaksahu <vinayaksahu3@gmail.com>
2024-02-27 16:18:24 +0530

    superkeyword with parameters

--- SuperKeyWord.java
+++ SuperKeyWord.java
@@ -4,25 +4,41 @@ class A {
 
     }
 
-    public void show() {
-        System.out.println("Hello A");
+    A (int num) {
+        System.out.println("parameterized SuperKeyWord constructor " + num);
+    }
+
+    public void show(int num ) {
+        System.out.println("Show Super " +num);
     }
 }
 
 public class SuperKeyWord extends A{
 
     SuperKeyWord () {
-        super();
+        //super(); //not necessary
+        System.out.println("default SuperKeyWord constructor");
+    }
+
+    SuperKeyWord (int num) {
+        super(100); //necessary
+        System.out.println("parameterized SuperKeyWord constructor " + num);
     }
     
-    public void show() {
-        super.show();
-        System.out.println("Hello SuperKeyWord");
+    public void show(int num) {
+        super.show(111);
+        System.out.println("show Sub " + num);
     }
     public static void main(String[] args) {
         
-        SuperKeyWord obj = new SuperKeyWord();
+        SuperKeyWord obj = new SuperKeyWord(); //default constructor
+        SuperKeyWord obj1 = new SuperKeyWord(10); // parameterized constructor 
 
-        obj.show();
+
+        System.out.println();
+        obj.show(11);
+        System.out.println();
+        obj1.show(12);
+
     }
 }
