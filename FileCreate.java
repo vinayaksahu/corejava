61f6fd1587e01bc0ebe44522f1c47c82f18ff03b
Vinayaksahu <vinayaksahu3@gmail.com>
2024-03-01 13:02:10 +0530

    file creation

---
+++ FileCreate.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreate {
+    public static void main(String[] args) throws IOException{
+        
+        File f = new File("C:\\Users\\vinayak\\Desktop\\vinayak.pptx");
+       // try{
+            if(f.createNewFile())
+                System.out.println("File created successfully");
+            else 
+                System.out.println("File already exits");
+       /*  } catch(IOException e) {
+            System.out.println("Exception Handled");
+        }*/
+
+    }
+}
