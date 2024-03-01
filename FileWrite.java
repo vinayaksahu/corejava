57782b24a7702dcaaf273b00edc47c2ab5d82a2a
Vinayaksahu <vinayaksahu3@gmail.com>
2024-03-01 14:26:29 +0530

    file writee

--- FileWrite.java
+++ FileWrite.java
@@ -1,19 +1,19 @@
 import java.io.*;
 
 public class FileWrite {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try {
+       // try {
             File f = new File("C:\\Users\\vinayak\\Desktop\\vinayak.txt");
             try(FileWriter fw = new FileWriter(f)) {
                 fw.write("Hello my name is Vinay.");
             }/*  finally {
                 fw.close();
             } */
-        } catch (Exception e) {
+      /*   } catch (Exception e) {
             //System.out.println(e);
             e.printStackTrace();
-        }
+        } */
         
 
         
