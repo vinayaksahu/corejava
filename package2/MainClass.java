3e992410c19c7b16efff628b371e1f616b9d5a3d
Vinayaksahu <vinayaksahu3@gmail.com>
2024-03-02 17:40:40 +0530

    tethered

--- package2/MainClass.java
+++ package2/MainClass.java
@@ -1,6 +1,12 @@
 package package2;
-
 import package1.A;
+
+class MainClass2 extends A {
+ 
+    public void show1() {
+        System.out.println("This is mainClas2 of package2 extends class A");
+    }
+}
 
 public class MainClass {
     public static void main(String[] args) {
